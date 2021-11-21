bbaf05008b6805c7fd97d1fe5fcd2b4e1e715753
Yasin Munye <cyasinsadiq@gmail.com>
2021-11-20 23:42:31 -0500

    moved questions to javascript file and developed some code. Added additional html pages for content.

---
+++ Develop/work.js
@@ -0,0 +1,34 @@
+var QandApile= [
+    {
+         question : 'Commonly used data types DO NOT include?',
+         option : ['Strings', 'Booleans','Alerts', 'Numbers'],
+         answer : 'alerts'
+    },
+    {
+         question : ' The condition in an if / else statement is enclosed within ____.',
+         option : ['Quotes', 'Curly brackets', 'Parentheses', 'Square brackets'],
+         answer : 'parentheses'
+    },
+    {
+         question : ' Arrays in JavaScript can be used to store ____.',
+         option : ['numbers and strings', 'other arrays', 'booleans', 'All of the above'],
+         answer : 'All of the above'
+    },
+    {
+         question : 'String values must be enclosed within ____ when being assigned to variables. ',
+         option : ['commas', 'curly brackets', 'quotes', 'parentheses'],
+         answer : 'quotes'
+    },
+    {
+         question : 'A very useful tool used during development and debugging for printing content to the debugger is:',
+         option : ['JavaScript','terminal / bash', 'for loops','console.log',]
+         answer : 'console.log'
+    }
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var choice0= document.getElementById('choice0');
+var choice1= document.getElementById('choice1');
+var choice2= document.getElementById('choice2');
+var choice3= document.getElementById('choice3');
