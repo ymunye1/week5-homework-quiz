0479704179aee8e05dc7c991d7d88b33fca8a670
Yasin Munye <cyasinsadiq@gmail.com>
2021-12-01 18:54:15 -0500

    added a countdown function, added a function to know which answers are right based on it being clicked, added a function where if answer is incorrect it is subracted from time

--- Develop/work.js
+++ Develop/work.js
@@ -2,12 +2,12 @@ var QandApile= [
     {
          question : 'Commonly used data types DO NOT include?',
          choice : ['Strings', 'Booleans','Alerts', 'Numbers'],
-         answer : 'alerts'
+         answer : 'Alerts'
     },
     {
          question : ' The condition in an if / else statement is enclosed within ____.',
          choice : ['Quotes', 'Curly brackets', 'Parentheses', 'Square brackets'],
-         answer : 'parentheses'
+         answer : 'Parentheses'
     },
     {
          question : ' Arrays in JavaScript can be used to store ____.',
@@ -33,19 +33,75 @@ var choice1= document.getElementById('choice1');
 var choice2= document.getElementById('choice2');
 var choice3= document.getElementById('choice3');
 var span= document.querySelector('span');
+var next= document.querySelector('.next');
+var elTime = document.getElementById("time-section");
+
+
+var secondsLeft = 100
+
+function setTime() {
+     var timer = setInterval(function(){
+     secondsLeft--;
+     elTime.textContent= "Your Score: " + secondsLeft;
+     
+          if (secondsLeft<=0) {
+               clearInterval(timer);
+               elTime.textContent= "Your Score: " +  "0";
+          }
+
+     },1000)
+}
+setTime()
+
 
 var i=0
-var score = 0;
+
 
 function showQuestion(){
-     for(var y=0; i<span.length; y++){
-          span[a].style.background='none';
+     for(var y=0; y<span.length; y++){
+      
      }
-     question.innerHTML= 'Q.'+(i+1)+' '+QandApile [i].question;
-     choice0.innerHTML= QandApile [i].choice[0];
-     choice1.innerHTML= QandApile [i].choice[1];
+     question.innerHTML= 'Question '+(i+1)+' # '+QandApile [i].question;
+     choice0.innerHTML= QandApile[i].choice[0];
+     choice1.innerHTML= QandApile[i].choice[1];
      choice2.innerHTML= QandApile[i].choice[2];
-     choice3. innerHTML= QandApile[i].choice[3];
-     stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+QandApile.length;
+     choice3.innerHTML= QandApile[i].choice[3];
+     whatQueNum.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+QandApile.length;
+     ansDisplay.innerHTML=" ";
+
+     }if(choice3.innerHTML= QandApile[i].choice[3]){
+     // stop(elTime);
 }
+
+
+function calcTime(t){
+     if(t.innerHTML===QandApile[i].answer){ 
+     
+          ansDisplay.innerHTML="Your Answer is:" + ' ' + 'Correct';
+          secondsLeft= secondsLeft;
+  
+     
+     }else{
+          secondsLeft= secondsLeft-20;
+ 
+          ansDisplay.innerHTML="Your Answer is:" + 'Incorrect';
+         
+     
+     }
+     
+     setTimeout(nextQuestion, 500);
+}
+
+function nextQuestion(){
+     if(i<QandApile.length-1)
+     {
+         i=i+1;
+         showQuestion();
+     }
+     else{
+         secondsLeft.innerHTML= score+'/'+
+         QandApile.length;
+     }
+}
+next.addEventListener('click', nextQuestion);
 showQuestion()
